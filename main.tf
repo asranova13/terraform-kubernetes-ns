19c6bd19cfee30fde7e09765a4bc24e82fa650c0
asranova13 <diana.asranova13@gmail.com>
2026-08-11 02:48:26 +0000

    added resource quoota

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
 resource "kubernetes_namespace_v1" "this" {
   metadata {
-      annotations = var.annotations
-      labels = var.labels
-      name = var.name
+    annotations = var.annotations
+    labels      = var.labels
+    name        = var.name
+  }
+}
+
+resource "kubernetes_resource_quota_v1" "pod_limit" {
+  metadata {
+    name      = "pod-limit"
+    namespace = kubernetes_namespace_v1.this.metadata[0].name
+  }
+
+  spec {
+    hard = {
+      pods = "100"
+    }
   }
 }
